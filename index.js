d04f99a756593777affe53335f0ef0cb5ccbb49c
夏明儒 <xiamingru@xiamingru.local>
2021-02-06 21:18:53 +0800

    feat: 第一版支持根据用户选择拉取远程模板

---
+++ index.js
@@ -0,0 +1,72 @@
+#!/usr/bin/env node
+
+const { program } = require('commander');
+const download = require('download-git-repo');
+const chalk = require('chalk');
+const ora = require('ora');
+const log = console.log;
+const inquirer = require('inquirer');
+
+const promptList = [
+    {
+        name: 'description',
+        message: '请输入项目描述'
+    },
+    {
+        name: 'projectType',
+        message: '请选择要初始化的项目类型',
+        type: 'list',
+        choices: [
+            "pc",
+            "h5"
+        ],
+    },
+    {
+        name: 'techType',
+        message: '请选择技术栈',
+        type: 'list',
+        choices: [
+            "vue",
+            "react",
+            "nodejs",
+        ],
+    }
+];
+const spinner = ora('奋力下载模板中...');
+
+
+program.version('1.0.0');
+
+// 自定义错误输出颜色
+program
+  .configureOutput({
+    // Output errors in red.
+    outputError: (str) => {
+        log(chalk.red(str));
+    }
+  });
+
+program.command('init <name>')
+       .description('init a project')
+       .action((name, opts) => {
+        inquirer.prompt(promptList).then(res => {
+            const {description, projectType, techType} = res;
+            if(!description){ // description缺省值设置
+                res.description = `a ${techType} ${projectType} project`;
+            }
+            spinner.start();
+            const downloadUrl = `direct:https://github.com/mingrutough1/project-init-templates.git#${techType}-${projectType}-template`;
+            download(downloadUrl, `${name}`, { clone: true }, (err) => {
+                if(err){
+                    spinner.fail('拉去失败请稍后再试');
+                    return;
+                }
+                spinner.succeed('模板下载成功');
+            });
+        })
+       });
+
+program.parse(process.argv);
+process.exit = (code) => {
+    console.log(`退出码: ${code}`);
+  };
